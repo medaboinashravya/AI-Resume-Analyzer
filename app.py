d5b56e1470bc7d75eaba64cfd53abaa301d40565
medaboinashravya <medaboinashravya161@gmail.com>
2026-07-05 21:56:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from PyPDF2 import PdfReader
 
 st.set_page_config(page_title="AI Resume Analyzer", page_icon="📄")
 
@@ -9,3 +10,12 @@ uploaded_file = st.file_uploader("Upload your Resume (PDF)", type=["pdf"])
 
 if uploaded_file is not None:
     st.success("Resume uploaded successfully!")
+
+    reader = PdfReader(uploaded_file)
+    text = ""
+
+    for page in reader.pages:
+        text += page.extract_text()
+
+    st.subheader("Extracted Resume Text")
+    st.write(text)
